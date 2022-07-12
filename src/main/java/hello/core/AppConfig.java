f2290a16485a9c1ce91e03556ad5a999c6e28875
EunHyeJung <ehye826@gmail.com>
2022-07-12 22:21:05 +0900

    컴포넌트 스캔과 의존관계 자동주입 test

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,21 @@ public class AppConfig {
     // MemberService 역할
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     // MemberRepository 역할
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     // OrderService 역할
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy() );
     }
 
---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // AppConfig 제외하기위해
+)
+public class AutoAppConfig {
+}
