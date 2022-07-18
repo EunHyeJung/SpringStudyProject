2eb9a6a644a112f852eb5aff9cdebe37fd448d8a
EunHyeJung <ehye826@gmail.com>
2022-07-18 15:52:09 +0900

    @Primary, @Qualifier 사용 테스트

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("fixDiscountPolicy")
+@Qualifier("FixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int disscountFixAmount = 1000; // 1000원 할인
